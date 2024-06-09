854f3aba43d4d1d65880ae2dcf3ca8ea56848f8a
2814109 <2814109k@gmail.com>
2024-06-09 12:11:53 +0900

    fix form

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -11,30 +11,12 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { useForm } from "@tanstack/react-form";
 import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
-import {
-  AlertCircle,
-  //LoaderCircle,
-  X,
-} from "lucide-react";
-// import { validateUsername } from "@/api/user";
-// import { zodValidator } from "@tanstack/zod-form-adapter";
-// import { z } from "zod";
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "./ui/select";
-
-// const UsernameSchema = z
-//   .string()
-//   .min(3, "Username must be at least 3 characters!!!");
+import { AlertCircle, ListPlus, SquarePlus, X } from "lucide-react";
 
 export const FeedbackForm = () => {
   const form = useForm({
     defaultValues: {
-      feedbacks: [] as {
+      feedbacks: [{ system: "", opinions: [{ context: "" }] }] as {
         system: string;
         opinions: { context: string }[];
       }[],
@@ -66,225 +48,28 @@ export const FeedbackForm = () => {
             e.stopPropagation();
           }}
         >
-          {/* <form.Field
-            name="username"
-            validatorAdapter={zodValidator}
-            validators={{
-              onChangeAsyncDebounceMs: 500,
-              onChangeAsync: ({ value }) => validateUsername(value),
-              onChange: UsernameSchema,
-            }}
-            children={(field) => (
-              <div>
-                <Label htmlFor="username">Username</Label>
-                <div className="relative">
-                  <Input
-                    id="username"
-                    type="text"
-                    value={field.state.value}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                  {field.getMeta().isValidating && (
-                    <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
-                      <LoaderCircle className="animate-spin" />
-                    </div>
-                  )}
-                </div>
-                {field.state.meta.errors && (
-                  <div className="text-red-500 text-sm mt-1">
-                    {field.state.meta.errors}
-                  </div>
-                )}
-              </div>
-            )}
-          />
-          <form.Field
-            name="password"
-            validators={{
-              onChange: ({ value }) => {
-                if (value.length < 6) {
-                  return "Password must be at least 6 characters long";
-                }
-
-                if (!/[A-Z]/.test(value)) {
-                  return "Password must contain at least one uppercase letter";
-                }
-
-                if (!/[a-z]/.test(value)) {
-                  return "Password must contain at least one lowercase letter";
-                }
-
-                if (!/[0-9]/.test(value)) {
-                  return "Password must contain at least one number";
-                }
-              },
-            }}
-            children={(field) => (
-              <div>
-                <Label htmlFor="password">Password</Label>
-                <Input
-                  id="password"
-                  type="password"
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                />
-                {field.state.meta.errors && (
-                  <div className="text-red-500 text-sm mt-1">
-                    {field.state.meta.errors}
-                  </div>
-                )}
-              </div>
-            )}
-          />
-          <form.Field
-            name="confirmPassword"
-            validators={{
-              onChangeListenTo: ["password"],
-              onChange: ({ value, fieldApi }) =>
-                value !== fieldApi.form.getFieldValue("password") &&
-                "Passwords do not match",
-            }}
-            children={(field) => (
-              <div>
-                <Label htmlFor="confirmPassword">Confirm Password</Label>
-                <Input
-                  id="confirmPassword"
-                  type="password"
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                />
-                {field.state.meta.errors && (
-                  <div className="text-red-500 text-sm mt-1">
-                    {field.state.meta.errors}
-                  </div>
-                )}
-              </div>
-            )}
-          />
-          <div>
-            <form.Field
-              name="interests"
-              mode="array"
-              children={(field) => (
-                <>
-                  <Label className="mr-2">Interests</Label>
-                  {field.state.value.map((_, index) => (
-                    <div key={index} className="flex gap-2 my-2">
-                      <Select
-                        value={`${index}`}
-                        onValueChange={(newIndex) =>
-                          field.moveValue(index, +newIndex)
-                        }
-                      >
-                        <SelectTrigger className="w-28">
-                          <SelectValue />
-                        </SelectTrigger>
-                        <SelectContent>
-                          {field.state.value.map((_, index) => (
-                            <SelectItem key={index} value={`${index}`}>
-                              # {index + 1}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <form.Field
-                        name={`interests[${index}]`}
-                        children={(subField) => (
-                          <Input
-                            type="text"
-                            value={subField.state.value}
-                            autoFocus
-                            onChange={(e) =>
-                              subField.handleChange(e.target.value)
-                            }
-                          />
-                        )}
-                      />
-                      <Button
-                        variant={"destructive"}
-                        onClick={() => field.removeValue(index)}
-                      >
-                        <X />
-                      </Button>
-                    </div>
-                  ))}
-                  <Button
-                    type="button"
-                    variant={"outline"}
-                    onClick={() => field.pushValue("")}
-                  >
-                    Add
-                  </Button>
-                </>
-              )}
-            />
-          </div>
-          <div>
-            <form.Field
-              name="skills"
-              mode="array"
-              children={(field) => (
-                <>
-                  <Label className="mr-2">Skills</Label>
-                  {field.state.value.map((_, index) => (
-                    <div key={index} className="flex gap-2 my-2">
-                      <form.Field
-                        name={`skills[${index}].language`}
-                        children={(subField) => (
-                          <Input
-                            type="text"
-                            value={subField.state.value}
-                            autoFocus
-                            onChange={(e) =>
-                              subField.handleChange(e.target.value)
-                            }
-                          />
-                        )}
-                      />
-                      <form.Field
-                        name={`skills[${index}].rating`}
-                        children={(subField) => (
-                          <Input
-                            type="number"
-                            value={subField.state.value}
-                            onChange={(e) =>
-                              subField.handleChange(e.target.valueAsNumber)
-                            }
-                          />
-                        )}
-                      />
-                      <Button
-                        variant={"destructive"}
-                        onClick={() => field.removeValue(index)}
-                      >
-                        <X />
-                      </Button>
-                    </div>
-                  ))}
-                  <Button
-                    type="button"
-                    variant={"outline"}
-                    onClick={() => field.pushValue({ language: "", rating: 0 })}
-                  >
-                    Add
-                  </Button>
-                </>
-              )}
-            />
-          </div> */}
           <div>
             <form.Field
               name="feedbacks"
               mode="array"
               children={(field) => (
                 <>
-                  <div>
+                  <div className="flex gap-2 items-center">
                     <Label>Feedback</Label>
+                    <ListPlus
+                      className="cursor-pointer"
+                      onClick={() =>
+                        field.pushValue({
+                          system: "",
+                          opinions: [{ context: "" }],
+                        })
+                      }
+                    />
                   </div>
                   {field.state.value.map((_, index) => (
-                    <div key={index}>
-                      <div className="flex gap-2 my-2 items-center">
-                        <Label className="mr-2">System Name</Label>
+                    <div key={index} className="py-4">
+                      <div className="flex gap-2 items-center">
+                        <Label className="mr-2">System</Label>
                         <form.Field
                           name={`feedbacks[${index}].system`}
                           children={(subField) => (
@@ -300,8 +85,18 @@ export const FeedbackForm = () => {
                         />
                       </div>
                       <div>
-                        <div>
+                        <div className="flex gap-2 items-center py-4">
                           <Label>Opinions</Label>
+                          <SquarePlus
+                            onClick={() =>
+                              field.form.pushFieldValue(
+                                `feedbacks[${index}].opinions`,
+                                {
+                                  context: "",
+                                }
+                              )
+                            }
+                          />
                         </div>
                         <div>
                           <form.Field
@@ -316,7 +111,6 @@ export const FeedbackForm = () => {
                                         <Input
                                           type="text"
                                           value={subField.state.value}
-                                          autoFocus
                                           onChange={(e) =>
                                             subField.handleChange(
                                               e.target.value
@@ -331,17 +125,6 @@ export const FeedbackForm = () => {
                                       onClick={() => field.removeValue(index)}
                                     >
                                       <X />
-                                    </Button>
-                                    <Button
-                                      type="button"
-                                      variant={"outline"}
-                                      onClick={() =>
-                                        subField.pushValue({
-                                          context: "add",
-                                        })
-                                      }
-                                    >
-                                      Add
                                     </Button>
                                   </div>
                                 ))}
@@ -358,18 +141,6 @@ export const FeedbackForm = () => {
                       </Button> */}
                     </div>
                   ))}
-                  <Button
-                    type="button"
-                    variant={"outline"}
-                    onClick={() =>
-                      field.pushValue({
-                        system: "",
-                        opinions: [{ context: "w" }],
-                      })
-                    }
-                  >
-                    Add Sysmtem Section
-                  </Button>
                 </>
               )}
             />
